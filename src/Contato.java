1c02e908f8ca7bb1ae0c1deee2f64ffa676e4acf
Daniel Carlotto <blackdanielcarlotto@gmail.com>
2022-04-19 22:08:00 -0400

    Cria classe contato

--- src/Contato.java
+++ src/Contato.java
@@ -1,3 +1,27 @@
 public class Contato {
+    private String _nome;
+    private String _sobrenome;
+    private String _telefone;
+
+    public Contato(String nome, String sobrenome, String telefone) {
+        _nome = nome;
+        _sobrenome = sobrenome;
+        _telefone = telefone;
+    }
+
+    public String get_nome() {
+        return _nome;
+    }
+    public String get_sobrenome() {
+        return _sobrenome;
+    }
+    public String get_telefone() {
+        return _telefone;
+    }
+    
+    @Override
+    public String toString(){
+        return "Nome: " + _nome + " " + _sobrenome + " - Telefone: " + _telefone;
+    }
     
 }
