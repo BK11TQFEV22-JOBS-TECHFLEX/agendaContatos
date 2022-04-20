e39df026f11f450ea77197efa41299b577a43547
Bernardo Esbérard <bernardbr@gmail.com>
2022-04-19 22:43:24 -0300

    :sparkles: Adiciona menu de cadastro de contatos.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+        do {
+            System.out.println("1 - Cadastrar contato");
+            System.out.println("2 - Listar contatos");
+            System.out.println("3 - Sair");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1: {
+                    System.out.println("Digite o nome do contato");
+                    String nome = scanner.next();
+                    System.out.println("Digite o sobrenome do contato");
+                    String sobrenome = scanner.next();
+                    System.out.println("Digite o telefone do contato");
+                    String telefone = scanner.next();
+
+                    System.out.println("Futuro contato - " + nome + " " + sobrenome + " " + telefone);
+
+                    break;
+                }
+                case 2: {
+                    System.out.println("Lista de contatos");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Saindo do sistema...");
+                    break;
+                }
+                default: {
+                    System.out.println("Opção inválida");
+                    break;
+                }
+            }
+
+        } while (opcao != 3 );
+
+        scanner.close();
     }
 }
